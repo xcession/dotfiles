3563540198673fcde0420774b83451e228842d11
Пэйнт <xcession@gmail.com>
2020-06-16 13:05:18 +0700

    Add Polybar configs

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Terminate already running bar instance
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+echo "---" | tee -a /tmp/polybar.log
+polybar mainbar-i3 >>/tmp/polybar.log 2>&1 &
+
+echo "Polybar launched..."
