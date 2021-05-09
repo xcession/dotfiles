29d67528d0a8a3e025bada1916afe5bf89ec9dea
Пэйнт <xcession@gmail.com>
2021-05-10 00:59:28 +0700

    Update polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
 echo "---" | tee -a /tmp/polybar.log
-polybar mainbar-i3 >>/tmp/polybar.log 2>&1 &
-polybar altbar-i3
+polybar i3-primary >>/tmp/polybar.log 2>&1 &
+polybar i3-secondary
 
 echo "Polybar launched..."
