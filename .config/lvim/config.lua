d484dff040ab39bbca67af09f9a6f25652c8a4b2
Пэйнт <xcession@gmail.com>
2022-02-23 16:27:01 +0700

    Disable style setting to make automatic style choosing properly work

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -61,7 +61,7 @@ lvim.builtin.nvimtree.quit_on_open = 1
 lvim.builtin.nvimtree.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 1
 
-vim.g.tokyonight_style = "night"
+-- vim.g.tokyonight_style = "night"
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
