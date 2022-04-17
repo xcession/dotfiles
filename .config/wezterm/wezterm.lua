747c72f326aeda12091789cdcb990b6ea6b92d16
Пэйнт <xcession@gmail.com>
2022-04-17 16:54:03 +0700

    Add wezterm config file

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm';
+return {
+  color_scheme = "synthwave",
+  font = wezterm.font_with_fallback({
+    "Iosevka Slab",
+    "Iosevka Nerd Font",
+    "Twemoji",
+  }),
+  font_size = 14,
+}
