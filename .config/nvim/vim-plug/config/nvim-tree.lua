671b5e7e3903df80844a6611de508e66d573aa38
Пэйнт <xcession@gmail.com>
2022-07-16 23:43:23 +0700

    Update nvim-tree config to the latest

--- .config/nvim/vim-plug/config/nvim-tree.lua
+++ .config/nvim/vim-plug/config/nvim-tree.lua
@@ -1,6 +1,10 @@
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-require'nvim-tree'.setup {
+require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
   auto_reload_on_write = true,
+  -- when creating files, sets the path of a file when cursor is on a closed folder to
+  -- the parent folder when false,
+  -- and inside the folder when true
+  create_in_closed_folder = false,
   disable_netrw = false,
   hijack_cursor = false,
   hijack_netrw = true,
@@ -10,8 +14,15 @@ require'nvim-tree'.setup {
   open_on_setup_file = false,
   open_on_tab = false,
   sort_by = "name",
-  update_cwd = false,
+  root_dirs = {},
+  prefer_startup_root = false,
+  sync_root_with_cwd = false,
+  reload_on_bufenter = false,
+  -- will change cwd of nvim-tree to that of new buffer's when opening nvim-tree
+  respect_buf_cwd = true,
   view = {
+    adaptive_size = false,
+    centralize_selection = false,
     width = 30,
     height = 30,
     hide_root_folder = false,
@@ -28,17 +39,79 @@ require'nvim-tree'.setup {
     },
   },
   renderer = {
+    -- append a trailing slash to folder names
+    add_trailing = true,
+    -- compact folders that only contain a single folder into one node in the file tree
+    group_empty = true,
+    -- will enable file highlight for git attributes (can be used without the icons)
+    highlight_git = true,
+    full_name = false,
+    -- enable folder and file icon highlight for opened files/directories
+    -- possible values: "none", "icon", "name", "all"
+    highlight_opened_files = "icon",
+    -- This is the default. See :help filename-modifiers for more options
+    root_folder_modifier = ":~",
     indent_markers = {
       enable = true,
       icons = {
-        corner = "└ ",
-        edge = "│ ",
-        none = "  ",
+        corner = "└",
+        edge = "│",
+        item = "│",
+        none = " ",
       },
     },
     icons = {
       webdev_colors = true,
+      git_placement = "before",
+      -- used for rendering the space between the icon and the filename
+      -- use with caution, it could break rendering if you set an empty string depending on your font
+      -- one space by default
+      padding = " ",
+      -- used as a separator between symlinks' source and target
+      symlink_arrow = " ➛ ",
+      -- it will only display if nvim-web-devicons is installed and on your runtimepath
+      -- if folder is true, you can also tell folder_arrows true to show small arrows next to the folder icons
+      -- but this will not work when you set renderer.indent_markers.enable (because of UI conflict)
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        bookmark = "",
+        folder = {
+          arrow_closed = "",
+          arrow_open = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+        git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "",
+          ignored = "◌",
+        },
+      },
     },
+    -- List of filenames that gets highlighted with NvimTreeSpecialFile
+    special_files = {
+      "Cargo.toml",
+      "Makefile",
+      "MAKEFILE",
+      "README.md",
+      "readme.md",
+    },
+    symlink_destination = true,
   },
   hijack_directories = {
     enable = true,
@@ -69,9 +142,15 @@ require'nvim-tree'.setup {
     custom = {},
     exclude = {},
   },
+  filesystem_watchers = {
+    enable = false,
+    interval = 100,
+    debounce_delay = 50,
+  },
   git = {
     enable = true,
     ignore = true,
+    show_on_dirs = true,
     timeout = 400,
   },
   actions = {
@@ -81,9 +160,13 @@ require'nvim-tree'.setup {
       global = false,
       restrict_above_cwd = false,
     },
+    expand_all = {
+      max_folder_discovery = 300,
+      exclude = {},
+    },
     open_file = {
       quit_on_open = true,
-      resize_window = false,
+      resize_window = true,
       window_picker = {
         enable = true,
         chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
@@ -93,10 +176,17 @@ require'nvim-tree'.setup {
         },
       },
     },
+    remove_file = {
+      close_window = true,
+    },
   },
   trash = {
-    cmd = "trash",
+    cmd = "gio trash",
     require_confirm = true,
+  },
+  live_filter = {
+    prefix = "[FILTER]: ",
+    always_show_folders = true,
   },
   log = {
     enable = false,
@@ -108,6 +198,7 @@ require'nvim-tree'.setup {
       diagnostics = false,
       git = false,
       profile = false,
+      watcher = false,
     },
   },
-}
+} -- END_DEFAULT_OPTS
