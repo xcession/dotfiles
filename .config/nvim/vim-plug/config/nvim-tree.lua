fad922b80c3ec15bcb56d5dd5cd43e56d2dd00c4
Пэйнт <xcession@gmail.com>
2022-10-13 19:03:38 +0700

    Update nvim-tree setting to reflect the latest updated config

--- .config/nvim/vim-plug/config/nvim-tree.lua
+++ .config/nvim/vim-plug/config/nvim-tree.lua
@@ -1,9 +1,6 @@
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
   auto_reload_on_write = true,
-  -- when creating files, sets the path of a file when cursor is on a closed folder to
-  -- the parent folder when false,
-  -- and inside the folder when true
   create_in_closed_folder = false,
   disable_netrw = false,
   hijack_cursor = false,
@@ -18,13 +15,14 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
   prefer_startup_root = false,
   sync_root_with_cwd = false,
   reload_on_bufenter = false,
-  -- will change cwd of nvim-tree to that of new buffer's when opening nvim-tree
-  respect_buf_cwd = true,
+  respect_buf_cwd = false,
+  on_attatch = "disable",
+  remove_keymaps = false,
+  select_prompts = false,
   view = {
     adaptive_size = false,
     centralize_selection = false,
     width = 30,
-    height = 30,
     hide_root_folder = false,
     side = "left",
     preserve_window_proportions = false,
@@ -39,39 +37,32 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
     },
   },
   renderer = {
-    -- append a trailing slash to folder names
+    -- appends a trailing slash to folder names.
     add_trailing = true,
-    -- compact folders that only contain a single folder into one node in the file tree
     group_empty = true,
-    -- will enable file highlight for git attributes (can be used without the icons)
     highlight_git = true,
     full_name = false,
-    -- enable folder and file icon highlight for opened files/directories
-    -- possible values: "none", "icon", "name", "all"
+    -- highlight icons and/or names for opened files.
+    -- value can be `"none"`, `"icon"`, `"name"` or `"all"`.
     highlight_opened_files = "icon",
-    -- This is the default. See :help filename-modifiers for more options
     root_folder_modifier = ":~",
+    indent_width = 2,
     indent_markers = {
       enable = true,
+      inline_arrows = true,
       icons = {
         corner = "└",
         edge = "│",
         item = "│",
+        bottom = "─",
         none = " ",
       },
     },
     icons = {
       webdev_colors = true,
       git_placement = "before",
-      -- used for rendering the space between the icon and the filename
-      -- use with caution, it could break rendering if you set an empty string depending on your font
-      -- one space by default
       padding = " ",
-      -- used as a separator between symlinks' source and target
       symlink_arrow = " ➛ ",
-      -- it will only display if nvim-web-devicons is installed and on your runtimepath
-      -- if folder is true, you can also tell folder_arrows true to show small arrows next to the folder icons
-      -- but this will not work when you set renderer.indent_markers.enable (because of UI conflict)
       show = {
         file = true,
         folder = true,
@@ -103,11 +94,9 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
         },
       },
     },
-    -- List of filenames that gets highlighted with NvimTreeSpecialFile
     special_files = {
       "Cargo.toml",
       "Makefile",
-      "MAKEFILE",
       "README.md",
       "readme.md",
     },
@@ -119,7 +108,7 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
   },
   update_focused_file = {
     enable = false,
-    update_cwd = false,
+    update_root = false,
     ignore_list = {},
   },
   ignore_ft_on_setup = {},
@@ -130,6 +119,7 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
   diagnostics = {
     enable = false,
     show_on_dirs = false,
+    debounce_delay = 50,
     icons = {
       hint = "",
       info = "",
@@ -143,7 +133,7 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
     exclude = {},
   },
   filesystem_watchers = {
-    enable = false,
+    enable = true,
     debounce_delay = 50,
   },
   git = {
@@ -162,6 +152,15 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
     expand_all = {
       max_folder_discovery = 300,
       exclude = {},
+    },
+    file_popup = {
+      open_win_config = {
+        col = 1,
+        row = 1,
+        relative = "cursor",
+        border = "shadow",
+        style = "minimal",
+      },
     },
     open_file = {
       quit_on_open = true,
@@ -194,6 +193,7 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
       all = false,
       config = false,
       copy_paste = false,
+      dev = false,
       diagnostics = false,
       git = false,
       profile = false,
