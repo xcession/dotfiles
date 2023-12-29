6939a1c31be9c3dd3578f6fc9ddd91c4b0c164a1
Пэйнт <xcession@gmail.com>
2023-12-29 17:20:07 +0700

    Update nvim-tree setting to reflect the latest updated config

--- .config/nvim/vim-plug/config/nvim-tree.lua
+++ .config/nvim/vim-plug/config/nvim-tree.lua
@@ -1,32 +1,47 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
+require("nvim-tree").setup {
+  -- OPTS
+  on_attach = "default",
+  hijack_cursor = false,
   auto_reload_on_write = true,
   disable_netrw = false,
-  hijack_cursor = false,
   hijack_netrw = true,
   hijack_unnamed_buffer_when_opening = false,
-  sort = {
-    sorter = "name",
-    folders_first = true,
-  },
   root_dirs = {},
   prefer_startup_root = false,
   sync_root_with_cwd = false,
   reload_on_bufenter = false,
   respect_buf_cwd = false,
-  on_attach = "default",
   select_prompts = false,
+  -- OPTS: SORT
+  sort = {
+    sorter = "name",
+    folders_first = true,
+    files_first = false,
+  },
+  -- OPTS: VIEW
   view = {
     centralize_selection = false,
     cursorline = true,
     debounce_delay = 15,
-    width = 30,
-    hide_root_folder = false,
     side = "left",
     preserve_window_proportions = false,
     number = false,
     relativenumber = false,
     signcolumn = "yes",
+    width = 30,
+    width = {
+      min = 30,
+      max = -1,
+      padding = 1,
+    },
     float = {
       enable = false,
       quit_on_focus_loss = true,
@@ -40,18 +55,26 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
       },
     },
   },
+  -- OPTS: RENDERER
   renderer = {
-    -- appends a trailing slash to folder names.
     add_trailing = true,
-    group_empty = true,
-    highlight_git = true,
+    group_empty = false,
     full_name = false,
-    -- highlight icons and/or names for opened files.
-    -- value can be `"none"`, `"icon"`, `"name"` or `"all"`.
-    highlight_opened_files = "icon",
-    highlight_modified = "none",
     root_folder_label = ":~:s?$?/..?",
     indent_width = 2,
+    special_files = {
+      "Cargo.toml",
+      "Makefile",
+      "README.md",
+      "readme.md",
+    },
+    symlink_destination = true,
+    highlight_git = true,
+    highlight_diagnostics = false,
+    highlight_opened_files = "icon",
+    highlight_modified = "none",
+    highlight_bookmarks = "none",
+    highlight_clipboard = "name",
     indent_markers = {
       enable = true,
       inline_arrows = true,
@@ -64,9 +87,21 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
       },
     },
     icons = {
-      webdev_colors = true,
+      --- webdev wip
+      web_devicons = {
+        file = {
+          enable = true,
+          color = true,
+        },
+        folder = {
+          enable = false,
+          color = true,
+        },
+      },
       git_placement = "before",
+      diagnostics_placement = "signcolumn",
       modified_placement = "after",
+      bookmarks_placement = "signcolumn",
       padding = " ",
       symlink_arrow = " ➛ ",
       show = {
@@ -75,11 +110,12 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
         folder_arrow = true,
         git = true,
         modified = true,
+        diagnostics = true,
+        bookmarks = true,
       },
       glyphs = {
         default = "",
         symlink = "",
-        bookmark = "",
         modified = "●",
         folder = {
           arrow_closed = "",
@@ -102,32 +138,38 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
         },
       },
     },
-    special_files = {
-      "Cargo.toml",
-      "Makefile",
-      "README.md",
-      "readme.md",
-    },
-    symlink_destination = true,
   },
+  -- OPTS: HIJACK DIRECTORIES
   hijack_directories = {
     enable = true,
     auto_open = true,
   },
+  -- OPTS: UPDATE FOCUSED FILE
   update_focused_file = {
     enable = false,
     update_root = false,
     ignore_list = {},
   },
+  -- OPTS: SYSTEM OPEN
   system_open = {
     cmd = "",
     args = {},
   },
+  -- OPTS: GIT
+  git = {
+    enable = true,
+    show_on_dirs = true,
+    show_on_open_dirs = true,
+    disable_for_dirs = {},
+    timeout = 400,
+    cygwin_support = false,
+  },
+  -- OPTS: DIAGNOSTICS
   diagnostics = {
     enable = false,
+    debounce_delay = 50,
     show_on_dirs = false,
     show_on_open_dirs = true,
-    debounce_delay = 50,
     severity = {
       min = vim.diagnostic.severity.HINT,
       max = vim.diagnostic.severity.ERROR,
@@ -139,31 +181,34 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
       error = "",
     },
   },
-  filters = {
-    git_ignored = true,
-    dotfiles = false,
-    git_clean = false,
-    no_buffer = false,
-    custom = {},
-    exclude = {},
-  },
-  filesystem_watchers = {
-    enable = true,
-    debounce_delay = 50,
-    ignore_dirs = {},
-  },
-  git = {
-    enable = true,
-    show_on_dirs = true,
-    show_on_open_dirs = true,
-    disable_for_dirs = {},
-    timeout = 400,
-  },
+  -- OPTS: MODIFIED
   modified = {
     enable = false,
     show_on_dirs = true,
     show_on_open_dirs = true,
   },
+  -- OPTS: FILTERS
+  filters = {
+    git_ignored = true,
+    dotfiles = false,
+    git_clean = false,
+    no_buffer = false,
+    no_bookmark = false,
+    custom = {},
+    exclude = {},
+  },
+  -- OPTS: LIVE FILTER
+  live_filter = {
+    prefix = "[FILTER]: ",
+    always_show_folders = true,
+  },
+  -- OPTS: FILESYSTEM WATCHERS
+  filesystem_watchers = {
+    enable = true,
+    debounce_delay = 50,
+    ignore_dirs = {},
+  },
+  -- OPTS: ACTIONS
   actions = {
     use_system_clipboard = true,
     change_dir = {
@@ -213,13 +258,11 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
       close_window = true,
     },
   },
+  -- OPTS: TRASH
   trash = {
     cmd = "gio trash",
   },
-  live_filter = {
-    prefix = "[FILTER]: ",
-    always_show_folders = true,
-  },
+  -- OPTS: TAB
   tab = {
     sync = {
       open = false,
@@ -227,32 +270,41 @@ require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
       ignore = {},
     },
   },
+  -- OPTS: NOTIFY
   notify = {
     threshold = vim.log.levels.INFO,
     absolute_path = true,
   },
+  -- OPTS: HELP
+  help = {
+    sort_by = "key",
+  },
+  -- OPTS: UI
   ui = {
     confirm = {
       remove = true,
       trash = true,
+      default_yes = false,
     },
   },
+  -- OPTS: EXPERIMENTAL
   experimental = {
     git = {
       async = true,
     },
   },
+  -- OPTS: LOG
   log = {
     enable = false,
     truncate = false,
     types = {
       all = false,
+      profile = false,
       config = false,
       copy_paste = false,
       dev = false,
       diagnostics = false,
       git = false,
-      profile = false,
       watcher = false,
     },
   },
